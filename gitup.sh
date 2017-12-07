39c86788f7231636f3c6a581486d1331360d3864
Wanghui <hfr2018@163.com>
2017-12-07 20:04:57 +0800

    m

--- gitup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bush
-git add -A
-git commit -m date
-git push -u hfr2018 --"Username for 'https://github.com' "=hfr2018@163.com --"Password for 'https://hfr2018@163.com@github.com"=husthfr2018
